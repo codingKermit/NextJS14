dca349ca2d8b09057fee16eea30a9738890fda81
codingKermit <woojw6012@gmail.com>
2024-10-22 15:07:52 +0900

    장비 변경으로 인한 코드 커밋

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+export default function Tomato(){
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export const metadata = {
+    title: 'Home | Next Movvies',
+    description: 'The best movies on the best framework',
+  }
