2eb5d613d329c6b7f4eb4020c50db9043ea736d0
codingKermit <woojw6012@gmail.com>
2024-10-24 20:38:58 +0900

    client component data fetching

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,27 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
 export default function Tomato(){
+    
+    const [isLoading,setIsLoading] = useState(true);
+
+    const [movies, setMovies] = useState();
+
+    const getMovies = async() =>{
+        const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
+        const json = await response.json();
+        setMovies(json);
+        setIsLoading(false);
+    }
+
+    useEffect(()=>{
+        getMovies();
+    },[])
+    
     return (
         <div>
-            <h1>Hello</h1>
+            {isLoading? "Loading..." : JSON.stringify(movies)}
         </div>
     )
 }
-
-export const metadata = {
-    title: 'Home | Next Movvies',
-    description: 'The best movies on the best framework',
-  }
