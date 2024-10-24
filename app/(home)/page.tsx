be5bd32a4c6b1a270a8340901cbabe8b581d40af
codingKermit <woojw6012@gmail.com>
2024-10-24 21:22:08 +0900

    server components data fetch

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,27 +1,24 @@
-"use client"
+// 서버 컴포넌트에서는 metadata도 사용 가능
+export const metadata = {
+    title : 'Home'
+}
 
-import { useEffect, useState } from "react"
+const url = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Tomato(){
-    
-    const [isLoading,setIsLoading] = useState(true);
+// data fetch할 async/await 문법의 함수 
+async function getMovies(){     
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+}
 
-    const [movies, setMovies] = useState();
-
-    const getMovies = async() =>{
-        const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
-        const json = await response.json();
-        setMovies(json);
-        setIsLoading(false);
-    }
-
-    useEffect(()=>{
-        getMovies();
-    },[])
-    
+// 화면을 그려줄 export default에도 async를 명시
+export default async function HomePage(){
+    // 내부에서 async/await 함수를 통한 data fetch
+    const movies = await getMovies();
     return (
         <div>
-            {isLoading? "Loading..." : JSON.stringify(movies)}
+            {JSON.stringify(movies)}
         </div>
     )
 }
