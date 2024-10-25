355e4ac948788bc156e2826501dec5368110c17e
codingKermit <woojw6012@gmail.com>
2024-10-25 21:52:09 +0900

    error Handling

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,10 +1,11 @@
 import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id:string){
-    // await new Promise((resolve)=>setTimeout(resolve,3_000));
-    const response = await fetch(`${API_URL}/${id}/videos`);
-    const json = response.json();
-    return json;
+    await new Promise((resolve)=>setTimeout(resolve,3_000));
+    throw new Error('Something broke...');
+    // const response = await fetch(`${API_URL}/${id}/videos`);
+    // const json = response.json();
+    // return json;
 }
 
 export default async function MovieVidoes({id}:{id:string}){
